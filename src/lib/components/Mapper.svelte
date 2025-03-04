c9cda754a16d17408f9cc416812dcc2fc7954453
ccalobeto <calobeto.leon@gmail.com>
2025-03-04 16:08:24 -0500

    refactor map component

---
+++ src/lib/components/Mapper.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { Map, MapSource, MapLayer, MapTooltip } from '@onsvisual/svelte-maps';
+
+	import { style } from '$lib/config.js';
+
+	export let filteredGeo;
+	export let bounds;
+	export let lookup;
+
+	let hovered;
+
+	let map = null;
+</script>
+
+{#if filteredGeo}
+	<div class="map-container">
+		<Map bind:map {style} controls location={{ bounds }}>
+			<MapSource id="geo" type="geojson" data={filteredGeo} promoteId="areacd">
+				<MapLayer
+					id="geo-fill"
+					type="fill"
+					paint={{
+						'fill-color': 'white',
+						'fill-opacity': ['case', ['==', ['feature-state', 'hovered'], true], 0.3, 0]
+					}}
+					hover
+					bind:hovered
+				>
+					<MapTooltip content={hovered ? `<b>${lookup[hovered].areanm}</b><br/>${hovered}` : ''} />
+				</MapLayer>
+				<MapLayer
+					id="geo-line"
+					type="line"
+					paint={{
+						'line-color': 'white',
+						'line-width': ['case', ['==', ['feature-state', 'hovered'], true], 2.5, 0.7]
+					}}
+				/>
+			</MapSource>
+		</Map>
+	</div>
+{/if}
+
+<style>
+	.map-container {
+		height: 500px;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,108 +1,21 @@
 <script>
-	import { onMount } from 'svelte';
-	import { feature } from 'topojson-client';
-	import { topology } from 'topojson-server';
-	import { csvFormat } from 'd3-dsv';
-	import bbox from '@turf/bbox';
-	import { coordEach } from '@turf/meta';
-	import { stringify } from 'wkt';
-
-	import { Map, MapSource, MapLayer, MapTooltip } from '@onsvisual/svelte-maps';
 	import Header from '$lib/components/Header.svelte';
-	import Footer from '../lib/components/Footer.svelte';
-	import Nav from '../lib/components/Nav.svelte';
-
-	import { geoTypes, countries, path, style } from '$lib/config.js';
-	import years from '$lib/years.js';
-
-	let map = null;
-	let bounds;
-	let hovered;
-	let lookup;
-	let geojson = {};
-	let geoType = geoTypes[0];
-	let year = years[years.length - 1];
-	let ctrys = [...countries];
-
-	function filterGeo(geo, year, ctrys) {
-		let filtered = JSON.parse(JSON.stringify(geo));
-		let codes = ['K', ...ctrys.map((c) => c.key)];
-		filtered.features = filtered.features
-			.filter((f) => {
-				return (
-					codes.includes(f.properties.areacd[0]) &&
-					!(f.properties.end && f.properties.end < year) &&
-					!(f.properties.start && f.properties.start > year)
-				);
-			})
-			.map((f) => {
-				f.properties = f.properties = { areacd: f.properties.areacd, areanm: f.properties.areanm };
-				return f;
-			});
-		return filtered;
-	}
-	$: filteredGeo = bounds ? filterGeo(geojson[geoType.key], year, ctrys) : null;
-
-	async function init() {
-		const topojson = await (await fetch(path)).json();
-
-		let lkp = {};
-		geoTypes.forEach((type) => {
-			geojson[type.key] = feature(topojson, topojson.objects[type.key]);
-			geojson[type.key].features.forEach((f) => (lkp[f.properties.areacd] = f.properties));
-		});
-		bounds = bbox(geojson['uk']);
-		lookup = lkp;
-	}
-	onMount(init);
+	import Footer from '$lib/components/Footer.svelte';
+	import Nav from '$lib/components/Nav.svelte';
 </script>
 
 <main>
 	<Header />
 	<Nav />
-
-	{#if filteredGeo}
-		<div class="map-container">
-			<Map bind:map {style} controls location={{ bounds }}>
-				<MapSource id="geo" type="geojson" data={filteredGeo} promoteId="areacd">
-					<MapLayer
-						id="geo-fill"
-						type="fill"
-						paint={{
-							'fill-color': 'white',
-							'fill-opacity': ['case', ['==', ['feature-state', 'hovered'], true], 0.3, 0]
-						}}
-						hover
-						bind:hovered
-					>
-						<MapTooltip
-							content={hovered ? `<b>${lookup[hovered].areanm}</b><br/>${hovered}` : ''}
-						/>
-					</MapLayer>
-					<MapLayer
-						id="geo-line"
-						type="line"
-						paint={{
-							'line-color': 'white',
-							'line-width': ['case', ['==', ['feature-state', 'hovered'], true], 2.5, 0.7]
-						}}
-					/>
-				</MapSource>
-			</Map>
-		</div>
-	{/if}
-
 	<Footer />
 </main>
 
 <style>
 	main {
+		background-color: pink;
+		border: 2px solid red;
 		max-width: 980px;
 		margin: 0 auto;
 		padding-bottom: 10px;
-	}
-
-	.map-container {
-		height: 500px;
 	}
 </style>
