5439120ffa6983ad07f6356fc9336827c8fbf6d4
ccalobeto <calobeto.leon@gmail.com>
2025-03-05 15:03:49 -0500

    update footer and header

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,7 +1,8 @@
 <p style:max-width="780px">
 	<small
-		>Esta herramienta fue desarrollada por <a href="https://www.carlosleon.dev/">Carlos León</a>
-		basado en los archivos cartográficos provistos por
+		>Esta herramienta fue desarrollada por <a href="https://www.carlosleon.dev/">Carlos León</a> en
+		<a href="https://svelte.dev/">Svelte</a>
+		y basado en los archivos cartográficos provistos por
 		<a href="https://www.idep.gob.pe/">Instituto Geográfico Nacional del Perú</a>. Puedes encontrar
 		el código utilizado para generar el archivo topojson en éste
 		<a href="https://github.com/ccalobeto/latam-atlas"> repositorio</a> de github.</small
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,9 @@
-<h1>Límites Geográficos Administrativos Perú - Ecuador</h1>
+<h1>Límites Cartográficos Administrativos del Perú</h1>
 
 <p style:max-width="780px">
-	Esta herramienta proporciona los límites geográficos de las principales cartografías
-	administrativas de Perú y Ecuador. Puedes ver y descargar un tipo de cartografía seleccionando el
-	menú desplegable a continuación, o usted puede descargar toda la cartografía en un <a
+	Esta herramienta proporciona los límites geográficos de las principales áreas administrativas del
+	Perú. Puedes ver y descargar un tipo de cartografía seleccionando el menú desplegable a
+	continuación, o usted puede descargar toda la cartografía en un <a
 		href="./data/peru-100k.json"
 		download="peru-100k.json">sólo archivo TopoJSON (1.9 MB)</a
 	>.
